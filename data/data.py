cfb727610b91e88c8abd217762b609a49cd09ab9
72craft <jin3472@hotmail.com>
2022-11-10 12:32:42 -0600

    removed ID and added IATA

--- data/data.py
+++ data/data.py
@@ -5,8 +5,8 @@ airports = pd.read_csv("airports.txt")
 routes = pd.read_csv("routes.txt")
 
 #drop all the unneeded columns
-airports = airports.drop(['City','Country','IATA','ICAO','Altitude','Timezone','DST','Tz','Type','Source'],axis = 1)
-routes = routes.drop(['Airline','Airline ID','Source airport','Destination airport','Codeshare','Stops','Equipment'], axis = 1)
+airports = airports.drop(['Airport ID','City','Country','ICAO','Altitude','Timezone','DST','Tz','Type','Source'],axis = 1)
+routes = routes.drop(['Airline','Airline ID','Source airport ID','Destination airport ID','Codeshare','Stops','Equipment'], axis = 1)
 
 #save the new data
 airports.to_csv("new_airports.txt")
